41974e1d51fd3a867c1e0e7abe0dd34557589d74
Daniel Rosel <60182044+danalves24com@users.noreply.github.com>
2021-05-12 15:18:59 +0200

    Add project files.

---
+++ TreebankInterpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreebankInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
